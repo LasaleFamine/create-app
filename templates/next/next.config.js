0c553001cab70b622010b0bf2c2f416905df97c4
LasaleFamine <info@godev.space>
2020-05-22 22:28:01 +0200

    feat: new env file support for next 9.4

--- templates/next/next.config.js
+++ templates/next/next.config.js
@@ -1,27 +1,13 @@
 /*
   eslint-disable
   @typescript-eslint/no-var-requires,
-  no-param-reassign,
   global-require
 */
 
-const {
-  PHASE_DEVELOPMENT_SERVER,
-} = require('next/constants');
-
-module.exports = (phase) => {
-  // Started in development mode `yarn dev` regardless of the value of STAGING environmental variable
-  const isDev = phase === PHASE_DEVELOPMENT_SERVER || process.env.NODE_ENV === 'development';
-
-  const env = {
-    IS_DEV: isDev,
-  };
-
+module.exports = () => {
   const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
   });
 
-  return withBundleAnalyzer({
-    env,
-  });
+  return withBundleAnalyzer();
 };
