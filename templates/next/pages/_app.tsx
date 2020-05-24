5460e5092c94db649e9bb0a0156d26decc0a2fc0
LasaleFamine <info@godev.space>
2020-05-24 11:01:49 +0200

    chore: better app.tsx

--- templates/next/pages/_app.tsx
+++ templates/next/pages/_app.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import '@native-elements/core';
 import './_app.css';
 
-export function reportWebVitals(metric) {
+export function reportWebVitals(metric: any) {
   // These metrics can be sent to any analytics service
-  console.log(metric)
+  console.log(metric);
 }
 
-const App = ({ pageProps, Component }) => (
+const App = ({ pageProps, Component }: any) => (
   <Component {...pageProps} />
-)
+);
 
 export default App;
